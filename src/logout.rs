f4fea32d7af639e6b3fc84e1c6c69e5e29098071
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-05-18 18:45:47 +0200

    added fist version of logout

---
+++ src/logout.rs
@@ -0,0 +1,112 @@
+use color_eyre::owo_colors::OwoColorize;
+use crate::{Icon, Message, Tab};
+use iced::{
+    alignment::{Horizontal, Vertical},
+    widget::{Button, Column, Container, Row, Text, TextInput},
+    Alignment, Element, Length,
+};
+use iced_aw::tab_bar::TabLabel;
+use iced_aw::{Card, Modal};
+use log::info;
+
+#[derive(Debug, Clone)]
+pub enum LogoutMessage {
+    OpenModal,
+    CloseModal,
+    CancelButtonPressed,
+    OkButtonPressed,
+}
+
+#[derive(Default)]
+pub struct LogoutTab {
+    show_modal: bool,
+    last_message: Option<LogoutMessage>,
+}
+
+impl LogoutTab {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn update(&mut self, message: LogoutMessage) {
+        match message {
+            LogoutMessage::OpenModal => self.show_modal = true,
+            LogoutMessage::CloseModal => self.show_modal = false,
+            LogoutMessage::CancelButtonPressed => self.show_modal = false,
+            LogoutMessage::OkButtonPressed => {
+                info!("Logout");
+                logout();
+                self.show_modal = false;
+            }
+        }
+        self.last_message = Some(message)
+    }
+}
+
+impl Tab for LogoutTab {
+    type Message = Message;
+
+    fn title(&self) -> String {
+        String::from("Logout")
+    }
+
+    fn tab_label(&self) -> TabLabel {
+        TabLabel::IconText(Icon::Logout.into(), self.title())
+    }
+
+    fn content(&self) -> Element<'_, Self::Message> {
+        let modal_content = Container::new(
+                Button::new(
+                    Text::new("Von Plantbuddy abmelden")
+                        .size(30)
+                        .height(Length::Fill)
+                        .width(Length::Fill),
+                )
+                .height(Length::from(100))
+                .width(Length::from(200))
+                .on_press(LogoutMessage::OpenModal),
+        )
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .center_x()
+        .center_y();
+
+        let content: Element<'_, LogoutMessage> =
+            Modal::new(self.show_modal, modal_content, || {
+                Card::new(
+                    Text::new("Abmeldung").size(40).horizontal_alignment(Horizontal::Center),
+                    Text::new("Wollen Sie sich wirklich von System abmelden?").size(30),
+                )
+                .foot(
+                    Row::new()
+                        .spacing(20)
+                        .padding(10)
+                        .width(Length::Fill)
+                        .push(
+                            Button::new(
+                                Text::new("Abbrechen").horizontal_alignment(Horizontal::Center).size(30),
+                            )
+                            .width(Length::Fill)
+                            .on_press(LogoutMessage::CancelButtonPressed),
+                        )
+                        .push(
+                            Button::new(Text::new("Ja").horizontal_alignment(Horizontal::Center).size(30))
+                                .width(Length::Fill)
+                                .on_press(LogoutMessage::OkButtonPressed),
+                        ),
+                )
+                .max_width(500.0)
+                .max_height(400.0)
+                .on_close(LogoutMessage::CloseModal)
+                .into()
+            })
+            .backdrop(LogoutMessage::CloseModal)
+            .on_esc(LogoutMessage::CloseModal)
+            .into();
+
+        content.map(Message::Logout)
+    }
+}
+fn logout() {
+    info!("userr logged out");
+}
