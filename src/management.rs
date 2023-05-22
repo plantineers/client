0ab864f413760bcd624636b6a0a79c3d15987487
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-05-23 00:40:37 +0200

    added user editing

--- src/management.rs
+++ src/management.rs
@@ -15,6 +15,7 @@ use iced_aw::TabLabel;
 use plotters::coord::types::RangedCoordf32;
 use plotters::prelude::*;
 use plotters_iced::{Chart, ChartBuilder, ChartWidget, DrawingBackend};
+use rand::random;
 
 #[derive(Debug, Clone)]
 pub enum ManagementMessage {
@@ -23,8 +24,12 @@ pub enum ManagementMessage {
     PasswordChanged(String),
     RoleChanged(PlantBuddyRole),
     CreateNewUser,
+    EditUserButton(User),
+    EditUser,
 }
+#[derive(Debug, Clone)]
 pub struct User {
+    id: u32,
     username: String,
     password: String,
     role: PlantBuddyRole,
@@ -35,6 +40,8 @@ pub(crate) struct ManagementTab {
     password_input: String,
     role_input: PlantBuddyRole,
     users: Vec<User>,
+    error_message: String,
+    editing_user: Option<User>,
 }
 
 impl ManagementTab {
@@ -44,6 +51,8 @@ impl ManagementTab {
             password_input: String::new(),
             role_input: PlantBuddyRole::User,
             users: Vec::new(),
+            error_message: String::new(),
+            editing_user: None,
         }
     }
 
@@ -51,25 +60,68 @@ impl ManagementTab {
         match message {
             ManagementMessage::UsernameChanged(username) => {
                 self.username_input = username;
+                self.error_message = String::new();
             }
             ManagementMessage::PasswordChanged(password) => {
                 self.password_input = password;
+                self.error_message = String::new();
             }
             ManagementMessage::CreateNewUser => {
-                self.users.push(User {
-                    username: self.username_input.clone(),
-                    password: self.password_input.clone(),
-                    role: self.role_input.clone(),
-                });
-                self.username_input.clear();
-                self.password_input.clear();
+                //Check if username or password is empty
+                if self.editing_user.is_none() {
+                    if self.username_input.is_empty() || self.password_input.is_empty() {
+                        self.error_message = String::from("Username or password is empty");
+                        return;
+                    }
+
+                    // TODO: Await backend answer
+                    self.users.push(User {
+                        id: random(),
+                        username: self.username_input.clone(),
+                        password: self.password_input.clone(),
+                        role: self.role_input.clone(),
+                    });
+                    self.username_input.clear();
+                    self.password_input.clear();
+                } else {
+                    if self.username_input.is_empty() || self.password_input.is_empty() {
+                        self.error_message = String::from("Username or password is empty");
+                        return;
+                    }
+                    let mut users = Vec::new();
+                    for mut user in self.users.clone() {
+                        if user.id == self.editing_user.clone().unwrap().id {
+                            user.username = self.username_input.clone();
+                            user.password = self.password_input.clone();
+                            user.role = self.role_input;
+                        }
+                        users.push(user);
+                    }
+
+                    self.username_input.clear();
+                    self.password_input.clear();
+                    self.users = users;
+                    self.editing_user = None;
+                }
             }
             ManagementMessage::DeleteUser => {
-                // Here you would need to determine which user to delete
-                // This is currently unimplemented
+                self.error_message = String::new();
+                // TODO: Await backend answer
+                // Check If user to delete is the current user
             }
             ManagementMessage::RoleChanged(role) => {
                 self.role_input = role;
+            }
+            ManagementMessage::EditUserButton(user) => {
+                self.error_message = String::new();
+                self.editing_user = Some(user.clone());
+
+                self.role_input = user.role.clone();
+                self.username_input = user.username.clone();
+                self.password_input = user.password.clone();
+            }
+            ManagementMessage::EditUser => {
+                self.error_message = String::new();
             }
         }
     }
@@ -90,8 +142,8 @@ impl Tab for ManagementTab {
         let column = Column::new()
             .spacing(20)
             .push(Text::new(self.title()).size(55))
-            .width(Length::from(600))
-            .height(Length::from(500))
+            .width(Length::Fill)
+            .height(Length::Fill)
             .align_items(Center)
             .push(self.content());
 
@@ -105,48 +157,64 @@ impl Tab for ManagementTab {
     }
 
     fn content(&self) -> Element<'_, Self::Message> {
-        let mut user_list = Column::new();
+        let mut user_list = Column::new().width(Length::from(1100)).height(Length::Fill);
         user_list = user_list.push(
             Row::new()
+                .spacing(20)
                 .push(
                     Container::new(Text::new("#"))
+                        .center_x()
+                        .center_y()
                         .padding(10)
                         .width(Length::from(50)),
                 )
                 .push(
                     Container::new(Text::new("Username"))
+                        .center_x()
+                        .center_y()
                         .padding(10)
-                        .width(Length::from(150)),
+                        .width(Length::from(300)),
                 )
                 .push(
                     Container::new(Text::new("Password"))
+                        .center_x()
+                        .center_y()
                         .padding(10)
-                        .width(Length::from(150)),
+                        .width(Length::from(200)),
                 )
                 .push(
                     Container::new(Text::new("Role"))
+                        .center_x()
+                        .center_y()
                         .padding(10)
-                        .width(Length::from(150)),
+                        .width(Length::from(200)),
                 )
-                .push(Container::new(Text::new("")))
-                .push(Container::new(Text::new(""))),
+                .push(Container::new(Text::new("")).width(Length::from(100)))
+                .push(Container::new(Text::new("")).width(Length::from(100))),
         );
         for (i, user) in self.users.iter().enumerate() {
             let row = Row::new()
+                .spacing(20)
                 .push(
-                    Container::new(Text::new(i.clone().to_string()))
+                    Container::new(Text::new(user.id.clone().to_string()))
+                        .center_x()
+                        .center_y()
                         .padding(10)
                         .width(Length::from(50)),
                 )
                 .push(
                     Container::new(Text::new(&user.username))
+                        .center_x()
+                        .center_y()
                         .padding(10)
-                        .width(Length::from(150)),
+                        .width(Length::from(300)),
                 )
                 .push(
                     Container::new(Text::new(&user.password))
+                        .center_x()
+                        .center_y()
                         .padding(10)
-                        .width(Length::from(150)),
+                        .width(Length::from(200)),
                 )
                 .push(
                     Container::new(Text::new(match &user.role {
@@ -154,11 +222,22 @@ impl Tab for ManagementTab {
                         PlantBuddyRole::Admin => "Admin",
                         _ => "This should not happen",
                     }))
+                    .center_x()
+                    .center_y()
                     .padding(10)
-                    .width(Length::from(50)),
+                    .width(Length::from(200)),
                 )
-                .push(Button::new(Text::new("Edit")).on_press(ManagementMessage::CreateNewUser))
-                .push(Button::new(Text::new("Delete")).on_press(ManagementMessage::DeleteUser));
+                .push(Container::new(
+                    Button::new(Text::new("Edit"))
+                        .on_press(ManagementMessage::EditUserButton(user.clone()))
+                        .width(Length::from(100)),
+                ))
+                .push(
+                    Container::new(
+                        Button::new(Text::new("Delete")).on_press(ManagementMessage::DeleteUser),
+                    )
+                    .width(Length::from(100)),
+                );
 
             user_list = user_list.push(row).push(Rule::horizontal(10));
         }
@@ -185,6 +264,7 @@ impl Tab for ManagementTab {
 
         let input_row = Row::new()
             .align_items(Center)
+            .spacing(20)
             .push(
                 TextInput::new("Username", &self.username_input)
                     .on_input(ManagementMessage::UsernameChanged),
@@ -194,10 +274,23 @@ impl Tab for ManagementTab {
                     .on_input(ManagementMessage::PasswordChanged),
             )
             .push(radio_column)
-            .push(Button::new("Create User").on_press(ManagementMessage::CreateNewUser));
+            .push(
+                Button::new(match self.editing_user {
+                    Some(_) => "Edit User",
+                    None => "Create User",
+                })
+                .on_press(ManagementMessage::CreateNewUser),
+            );
 
-        let content: Element<'_, ManagementMessage> =
-            Column::new().push(scrollable).push(input_row).into();
+        let content: Element<'_, ManagementMessage> = Column::new()
+            .push(scrollable)
+            .push(if self.error_message != String::new() {
+                Text::new(&self.error_message).style(Color::from_rgb(1.0, 0.0, 0.0))
+            } else {
+                Text::new("")
+            })
+            .push(input_row)
+            .into();
 
         content.map(Message::Management)
     }
